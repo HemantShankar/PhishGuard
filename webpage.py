9fd89d85788cc2609fe28055691885cddffda0ab
HemantShankar <hemantshankar9595@gmail.com>
2024-10-15 10:25:23 +0530

    removed unnecessary library

--- webpage.py
+++ webpage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle  
 import pandas as pd
 import numpy as np
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
 from url_script_main import featureExtraction
 
 
